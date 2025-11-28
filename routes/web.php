6db6adc21375a0c1571e9795f648ba3e3363e0c5
azankhokhar47 <azankhokhar203@gmail.com>
2025-11-28 16:06:18 +0500

    update the code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', function () {
+    // return view('post');
+    return "<h1>post page</h1>";
+});
+
+// Route::view('post','/post');
+
+// Route::get('hello', function () {
+//     return view('post');
+// });
