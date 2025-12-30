147879209a56b3bc4de763b1a12f8c3e4cdea258
azankhokhar47 <azankhokhar203@gmail.com>
2025-12-30 16:49:19 +0500

    new code

--- app/Http/Controllers/Testingcontroller.php
+++ app/Http/Controllers/Testingcontroller.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Contrllers;
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class TestingController extends Controller
 {
     public function __invoke(Request $request)
     {
-       return('Testing');
+       return view('Testing');
     }
 }
--- config/database.php
+++ config/database.php
@@ -27,7 +27,7 @@ return [
     | An example configuration is provided for each database system which
     | is supported by Laravel. You're free to add / remove connections.
     |
-    */
+    */ 
 
     'connections' => [
 
