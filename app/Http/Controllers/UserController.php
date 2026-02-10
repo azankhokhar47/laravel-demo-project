c97c84ca54515280a0f11bc28f13aba059dbc88b
azankhokhar47 <azankhokhar203@gmail.com>
2026-02-10 15:18:50 +0500

    new code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function addUser(Request $req){
+    public function addUser(UserRequest $req){
 
 
-    $req->validate([
-     'username'=> 'required',
-     'useremail'=> 'required|email',
-     'userage'=> 'required|numeric|min:18',
-     'usercity'=> 'required',
-    ],[
-        "username.required" => 'User name is required',
-        "useremail.required" => 'User email is required',
-        "useremail.required" => 'User email is Invalid',
-        "userage.required" => 'User age is required',
-        "userage.required" => 'User age is must be number',
-        "userage.required" => 'User age is less than 18',
-        "usercity.required" => 'User city is required',
-    ]);
+    // $req->validate([
+    //  'username'=> 'required',
+    //  'useremail'=> 'required|email',
+    //  'userage'=> 'required|numeric|min:18',
+    //  'usercity'=> 'required',
+    // ],[
+    //     "username.required" => 'User name is required',
+    //     "useremail.required" => 'User email is required',
+    //     "useremail.required" => 'User email is Invalid',
+    //     "userage.required" => 'User age is required',
+    //     "userage.required" => 'User age is must be number',
+    //     "userage.required" => 'User age is less than 18',
+    //     "usercity.required" => 'User city is required',
+    // ]);
         return $req->all();
     }
 }
